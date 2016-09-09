89908e0c5587ed1419c32af72f4ac04a193fc449
Dave Airlie <airlied@redhat.com>
2016-09-09 08:01:48 +0100

    radv/cmask: if we can't fast clear initialise the cmask and color clear regs
    So if we fail to do the clear we still need to put the valid cmask in place
    for a slow clear, and zero the reg storage.

--- src/amd/vulkan/radv_cmd_buffer.c
+++ src/amd/vulkan/radv_cmd_buffer.c
@@ -2027,14 +2027,14 @@ static void radv_handle_depth_image_transition(struct radv_cmd_buffer *cmd_buffe
 	}
 }
 
-static void radv_initialise_cmask(struct radv_cmd_buffer *cmd_buffer,
-				  struct radv_image *image)
+void radv_initialise_cmask(struct radv_cmd_buffer *cmd_buffer,
+			   struct radv_image *image)
 {
 	cmd_buffer->state.flush_bits |= RADV_CMD_FLAG_FLUSH_AND_INV_CB |
 	                                RADV_CMD_FLAG_FLUSH_AND_INV_CB_META;
 
 	radv_fill_buffer(cmd_buffer, image->bo->bo, image->offset + image->cmask.offset,
-			 image->cmask.size, 0xcccccccc);
+			 image->cmask.size, 0xccccccccu);
 
 	cmd_buffer->state.flush_bits |= RADV_CMD_FLAG_FLUSH_AND_INV_CB_META |
 	                                RADV_CMD_FLAG_CS_PARTIAL_FLUSH |
@@ -2052,7 +2052,7 @@ static void radv_handle_cmask_image_transition(struct radv_cmd_buffer *cmd_buffe
 	if (radv_layout_has_cmask(image, src_layout) &&
 	    !radv_layout_has_cmask(image, dst_layout)) {
 		radv_fast_clear_flush_image_inplace(cmd_buffer, image);
-	} else if (0 && !radv_layout_has_cmask(image, src_layout) &&
+	} else if (!radv_layout_has_cmask(image, src_layout) &&
 		   radv_layout_has_cmask(image, dst_layout)) {
 		radv_initialise_cmask(cmd_buffer, image);
 	}
