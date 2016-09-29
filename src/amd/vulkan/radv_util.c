216234c0944d23909b3ed90044694b2b45253b89
Dave Airlie <airlied@redhat.com>
2016-09-29 10:38:30 +1000

    radv: reformat radv_util.c

--- src/amd/vulkan/radv_util.c
+++ src/amd/vulkan/radv_util.c
@@ -34,171 +34,171 @@
 
 /** Log an error message.  */
 void radv_printflike(1, 2)
-radv_loge(const char *format, ...)
+	radv_loge(const char *format, ...)
 {
-   va_list va;
+	va_list va;
 
-   va_start(va, format);
-   radv_loge_v(format, va);
-   va_end(va);
+	va_start(va, format);
+	radv_loge_v(format, va);
+	va_end(va);
 }
 
 /** \see radv_loge() */
 void
 radv_loge_v(const char *format, va_list va)
 {
-   fprintf(stderr, "vk: error: ");
-   vfprintf(stderr, format, va);
-   fprintf(stderr, "\n");
+	fprintf(stderr, "vk: error: ");
+	vfprintf(stderr, format, va);
+	fprintf(stderr, "\n");
 }
 
 void radv_printflike(3, 4)
-__radv_finishme(const char *file, int line, const char *format, ...)
+	__radv_finishme(const char *file, int line, const char *format, ...)
 {
-   va_list ap;
-   char buffer[256];
+	va_list ap;
+	char buffer[256];
 
-   va_start(ap, format);
-   vsnprintf(buffer, sizeof(buffer), format, ap);
-   va_end(ap);
+	va_start(ap, format);
+	vsnprintf(buffer, sizeof(buffer), format, ap);
+	va_end(ap);
 
-   fprintf(stderr, "%s:%d: FINISHME: %s\n", file, line, buffer);
+	fprintf(stderr, "%s:%d: FINISHME: %s\n", file, line, buffer);
 }
 
 void radv_noreturn radv_printflike(1, 2)
-radv_abortf(const char *format, ...)
+	radv_abortf(const char *format, ...)
 {
-   va_list va;
+	va_list va;
 
-   va_start(va, format);
-   radv_abortfv(format, va);
-   va_end(va);
+	va_start(va, format);
+	radv_abortfv(format, va);
+	va_end(va);
 }
 
 void radv_noreturn
 radv_abortfv(const char *format, va_list va)
 {
-   fprintf(stderr, "vk: error: ");
-   vfprintf(stderr, format, va);
-   fprintf(stderr, "\n");
-   abort();
+	fprintf(stderr, "vk: error: ");
+	vfprintf(stderr, format, va);
+	fprintf(stderr, "\n");
+	abort();
 }
 
 VkResult
 __vk_errorf(VkResult error, const char *file, int line, const char *format, ...)
 {
-   va_list ap;
-   char buffer[256];
+	va_list ap;
+	char buffer[256];
 
 #define ERROR_CASE(error) case error: error_str = #error; break;
 
-   const char *error_str;
-   switch ((int32_t)error) {
+	const char *error_str;
+	switch ((int32_t)error) {
 
-   /* Core errors */
-   ERROR_CASE(VK_ERROR_OUT_OF_HOST_MEMORY)
-   ERROR_CASE(VK_ERROR_OUT_OF_DEVICE_MEMORY)
-   ERROR_CASE(VK_ERROR_INITIALIZATION_FAILED)
-   ERROR_CASE(VK_ERROR_DEVICE_LOST)
-   ERROR_CASE(VK_ERROR_MEMORY_MAP_FAILED)
-   ERROR_CASE(VK_ERROR_LAYER_NOT_PRESENT)
-   ERROR_CASE(VK_ERROR_EXTENSION_NOT_PRESENT)
-   ERROR_CASE(VK_ERROR_INCOMPATIBLE_DRIVER)
+		/* Core errors */
+		ERROR_CASE(VK_ERROR_OUT_OF_HOST_MEMORY)
+			ERROR_CASE(VK_ERROR_OUT_OF_DEVICE_MEMORY)
+			ERROR_CASE(VK_ERROR_INITIALIZATION_FAILED)
+			ERROR_CASE(VK_ERROR_DEVICE_LOST)
+			ERROR_CASE(VK_ERROR_MEMORY_MAP_FAILED)
+			ERROR_CASE(VK_ERROR_LAYER_NOT_PRESENT)
+			ERROR_CASE(VK_ERROR_EXTENSION_NOT_PRESENT)
+			ERROR_CASE(VK_ERROR_INCOMPATIBLE_DRIVER)
 
-   /* Extension errors */
-   ERROR_CASE(VK_ERROR_OUT_OF_DATE_KHR)
+			/* Extension errors */
+			ERROR_CASE(VK_ERROR_OUT_OF_DATE_KHR)
 
-   default:
-      assert(!"Unknown error");
-      error_str = "unknown error";
-   }
+	default:
+		assert(!"Unknown error");
+		error_str = "unknown error";
+	}
 
 #undef ERROR_CASE
 
-   if (format) {
-      va_start(ap, format);
-      vsnprintf(buffer, sizeof(buffer), format, ap);
-      va_end(ap);
+	if (format) {
+		va_start(ap, format);
+		vsnprintf(buffer, sizeof(buffer), format, ap);
+		va_end(ap);
 
-      fprintf(stderr, "%s:%d: %s (%s)\n", file, line, buffer, error_str);
-   } else {
-      fprintf(stderr, "%s:%d: %s\n", file, line, error_str);
-   }
+		fprintf(stderr, "%s:%d: %s (%s)\n", file, line, buffer, error_str);
+	} else {
+		fprintf(stderr, "%s:%d: %s\n", file, line, error_str);
+	}
 
-   return error;
+	return error;
 }
 
 int
 radv_vector_init(struct radv_vector *vector, uint32_t element_size, uint32_t size)
 {
-   assert(util_is_power_of_two(size));
-   assert(element_size < size && util_is_power_of_two(element_size));
+	assert(util_is_power_of_two(size));
+	assert(element_size < size && util_is_power_of_two(element_size));
 
-   vector->head = 0;
-   vector->tail = 0;
-   vector->element_size = element_size;
-   vector->size = size;
-   vector->data = malloc(size);
+	vector->head = 0;
+	vector->tail = 0;
+	vector->element_size = element_size;
+	vector->size = size;
+	vector->data = malloc(size);
 
-   return vector->data != NULL;
+	return vector->data != NULL;
 }
 
 void *
 radv_vector_add(struct radv_vector *vector)
 {
-   uint32_t offset, size, split, src_tail, dst_tail;
-   void *data;
+	uint32_t offset, size, split, src_tail, dst_tail;
+	void *data;
 
-   if (vector->head - vector->tail == vector->size) {
-      size = vector->size * 2;
-      data = malloc(size);
-      if (data == NULL)
-         return NULL;
-      src_tail = vector->tail & (vector->size - 1);
-      dst_tail = vector->tail & (size - 1);
-      if (src_tail == 0) {
-         /* Since we know that the vector is full, this means that it's
-          * linear from start to end so we can do one copy.
-          */
-         memcpy(data + dst_tail, vector->data, vector->size);
-      } else {
-         /* In this case, the vector is split into two pieces and we have
-          * to do two copies.  We have to be careful to make sure each
-          * piece goes to the right locations.  Thanks to the change in
-          * size, it may or may not still wrap around.
-          */
-         split = align_u32(vector->tail, vector->size);
-         assert(vector->tail <= split && split < vector->head);
-         memcpy(data + dst_tail, vector->data + src_tail,
-                split - vector->tail);
-         memcpy(data + (split & (size - 1)), vector->data,
-                vector->head - split);
-      }
-      free(vector->data);
-      vector->data = data;
-      vector->size = size;
-   }
+	if (vector->head - vector->tail == vector->size) {
+		size = vector->size * 2;
+		data = malloc(size);
+		if (data == NULL)
+			return NULL;
+		src_tail = vector->tail & (vector->size - 1);
+		dst_tail = vector->tail & (size - 1);
+		if (src_tail == 0) {
+			/* Since we know that the vector is full, this means that it's
+			 * linear from start to end so we can do one copy.
+			 */
+			memcpy(data + dst_tail, vector->data, vector->size);
+		} else {
+			/* In this case, the vector is split into two pieces and we have
+			 * to do two copies.  We have to be careful to make sure each
+			 * piece goes to the right locations.  Thanks to the change in
+			 * size, it may or may not still wrap around.
+			 */
+			split = align_u32(vector->tail, vector->size);
+			assert(vector->tail <= split && split < vector->head);
+			memcpy(data + dst_tail, vector->data + src_tail,
+			       split - vector->tail);
+			memcpy(data + (split & (size - 1)), vector->data,
+			       vector->head - split);
+		}
+		free(vector->data);
+		vector->data = data;
+		vector->size = size;
+	}
 
-   assert(vector->head - vector->tail < vector->size);
+	assert(vector->head - vector->tail < vector->size);
 
-   offset = vector->head & (vector->size - 1);
-   vector->head += vector->element_size;
+	offset = vector->head & (vector->size - 1);
+	vector->head += vector->element_size;
 
-   return vector->data + offset;
+	return vector->data + offset;
 }
 
 void *
 radv_vector_remove(struct radv_vector *vector)
 {
-   uint32_t offset;
+	uint32_t offset;
 
-   if (vector->head == vector->tail)
-      return NULL;
+	if (vector->head == vector->tail)
+		return NULL;
 
-   assert(vector->head - vector->tail <= vector->size);
+	assert(vector->head - vector->tail <= vector->size);
 
-   offset = vector->tail & (vector->size - 1);
-   vector->tail += vector->element_size;
+	offset = vector->tail & (vector->size - 1);
+	vector->tail += vector->element_size;
 
-   return vector->data + offset;
+	return vector->data + offset;
 }
