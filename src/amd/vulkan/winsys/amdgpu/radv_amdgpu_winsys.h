76ef11d49dccef3758c9bc2937a4061f5ba11d31
Dave Airlie <airlied@redhat.com>
2016-10-04 11:28:00 +1000

    radv/winsys: import the amdgpu winsys for the radv vulkan driver.
    This just brings these files into the tree, it doesn't integrate
    them with the build system.
    
    The radv winsys is based on the gallium one with some changes,
    due to how command buffers are built and lack of flushing behaviour.
    
    Authors: Bas Nieuwenhuizen and Dave Airlie
    Signed-off-by: Dave Airlie <airlied@redhat.com>

---
+++ src/amd/vulkan/winsys/amdgpu/radv_amdgpu_winsys.h
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include "radv_radeon_winsys.h"
+#include "addrlib/addrinterface.h"
+#include <amdgpu.h>
+#include "util/list.h"
+
+struct radv_amdgpu_winsys {
+	struct radeon_winsys base;
+	amdgpu_device_handle dev;
+
+	struct radeon_info info;
+	struct amdgpu_gpu_info amdinfo;
+	ADDR_HANDLE addrlib;
+
+	uint32_t rev_id;
+	unsigned family;
+
+	bool debug_all_bos;
+	pthread_mutex_t global_bo_list_lock;
+	struct list_head global_bo_list;
+	unsigned num_buffers;
+
+	bool use_ib_bos;
+};
+
+static inline struct radv_amdgpu_winsys *
+radv_amdgpu_winsys(struct radeon_winsys *base)
+{
+	return (struct radv_amdgpu_winsys*)base;
+}
