c28c71b790be4ccc3c8a70fd95ce8b4383ea542c
Dave Airlie <airlied@redhat.com>
2016-12-28 00:33:07 +0000

    spirv: add support for SPV_AMD_shader_trinary_minmax
    This is one of 3 AMD extensions that DOOM can use.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>
    (cherry picked from commit c2f47d5df5364695232da280acfe95e5e8118f12)

--- src/compiler/spirv/nir_spirv.h
+++ src/compiler/spirv/nir_spirv.h
@@ -57,6 +57,7 @@ struct nir_spirv_supported_extensions {
    bool amd_shader_ballot;
    bool groups;
    bool amd_gcn_shader;
+   bool amd_shader_trinary_minmax;
 };
 
 nir_function *spirv_to_nir(const uint32_t *words, size_t word_count,
