5aa83e136dbe22727eb2075e49b8da5ef52e90f3
Dave Airlie <airlied@redhat.com>
2017-02-15 04:39:43 +0000

    spirv: add support for Int64 capability
    This just adds the support at the spirv->nir level for the Int64
    cap.
    
    Signed-off-by: Dave Airlie <airlied@redhat.com>

--- src/compiler/spirv/nir_spirv.h
+++ src/compiler/spirv/nir_spirv.h
@@ -50,6 +50,7 @@ struct nir_spirv_supported_extensions {
    bool tessellation;
    bool draw_parameters;
    bool image_write_without_format;
+   bool int64;
 };
 
 nir_function *spirv_to_nir(const uint32_t *words, size_t word_count,
