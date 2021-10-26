f106d4e4d29d49c713515d13517c8d9f1a46b6f0
Dave Airlie <airlied@redhat.com>
2021-10-26 10:51:31 +1000

    radv/video: add initial h264 decoder for VCN
    This adds support for H264 decode on VCN hardware.
    
    It uses the full DPB method, and relies on the application
    to allocate an arrayed texture for the DPB to be stored into.
    
    RADV_VIDEO_DECODE=1 is required to enable this.

--- src/amd/vulkan/radv_image.c
+++ src/amd/vulkan/radv_image.c
@@ -48,6 +48,9 @@ radv_choose_tiling(struct radv_device *device, const VkImageCreateInfo *pCreateI
       return RADEON_SURF_MODE_LINEAR_ALIGNED;
    }
 
+   if (pCreateInfo->usage & (VK_IMAGE_USAGE_VIDEO_DECODE_DST_BIT_KHR | VK_IMAGE_USAGE_VIDEO_DECODE_DPB_BIT_KHR))
+      return RADEON_SURF_MODE_LINEAR_ALIGNED;
+
    /* MSAA resources must be 2D tiled. */
    if (pCreateInfo->samples > 1)
       return RADEON_SURF_MODE_2D;
@@ -517,15 +520,20 @@ radv_patch_image_from_extra_info(struct radv_device *device, struct radv_image *
                                  struct ac_surf_info *image_info)
 {
    VkResult result = radv_patch_image_dimensions(device, image, create_info, image_info);
+   bool add_shareable = false;
    if (result != VK_SUCCESS)
       return result;
 
+   if (image->plane_count > 1)
+      add_shareable = true;
    for (unsigned plane = 0; plane < image->plane_count; ++plane) {
       if (create_info->bo_metadata) {
          radv_patch_surface_from_metadata(device, &image->planes[plane].surface,
                                           create_info->bo_metadata);
       }
 
+      if (add_shareable)
+         image->planes[plane].surface.flags |= RADEON_SURF_SHAREABLE;
       if (radv_surface_has_scanout(device, create_info)) {
          image->planes[plane].surface.flags |= RADEON_SURF_SCANOUT;
          if (device->instance->debug_flags & RADV_DEBUG_NO_DISPLAY_DCC)
