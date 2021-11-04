c25489f21ef4cb8e7d50dd022c3a71d0dcd5bec4
Dave Airlie <airlied@redhat.com>
2021-11-05 09:24:22 +1000

    fix frame decoding

--- src/amd/vulkan/radv_video.c
+++ src/amd/vulkan/radv_video.c
@@ -956,7 +956,7 @@ static rvcn_dec_message_avc_t get_h264_msg(struct radv_device *device,
 
    result.frame_num = h264_pic_info->pStdPictureInfo->frame_num;
 
-   result.num_ref_frames = frame_info->referenceSlotCount;
+   result.num_ref_frames = sps->max_num_ref_frames;
    for (unsigned i = 0; i < frame_info->referenceSlotCount; i++) {
       int idx = frame_info->pReferenceSlots[i].slotIndex;
       const struct VkVideoDecodeH264DpbSlotInfoEXT *dpb_slot =
