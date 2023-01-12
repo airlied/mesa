15f4ba0663132ae78c8b92d3c5388cca3778e286
Dave Airlie <airlied@redhat.com>
2023-01-12 18:14:42 +1000

    workaround

--- src/vulkan/runtime/vk_video.c
+++ src/vulkan/runtime/vk_video.c
@@ -231,7 +231,8 @@ vk_video_session_parameters_init(struct vk_device *device,
    case VK_VIDEO_CODEC_OPERATION_DECODE_AV1_BIT_MESA: {
       const struct VkVideoDecodeAV1SessionParametersCreateInfoMESA *av1_create =
          vk_find_struct_const(create_info->pNext, VIDEO_DECODE_AV1_SESSION_PARAMETERS_CREATE_INFO_MESA);
-      params->av1_dec.seq_hdr = *av1_create->pParametersAddInfo->sequence_header;
+      if (av1_create)
+         params->av1_dec.seq_hdr = *av1_create->pParametersAddInfo->sequence_header;
       break;
    }
    default:
